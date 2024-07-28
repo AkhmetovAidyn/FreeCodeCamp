10d5379db5c0a69e33acf85ce7e69f1f4746f782
AkhmetovAidyn <ahmetovv_aidyn@mail.ru>
2024-07-28 19:12:09 +0500

    pyramid-generator-21.js

---
+++ learn-intoductory-js-building-a-pyramid-generator/pyramid-generator-21.js
@@ -0,0 +1,14 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+
+// Step 21
+// Arrays are special in that they are considered mutable. This means you can change the value at an index directly.
+// For example, this code would assign the number 25 to the second element in the array:
+// Example Code
+// Update the third element of your rows array to be the number 10.
+// Then print the rows array to your console.
+
+rows[2] = 10;
+console.log(rows);
