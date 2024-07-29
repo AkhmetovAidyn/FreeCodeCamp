83907c3b37f7e2edf499a9ea4a946d50b66b99e3
AkhmetovAidyn <ahmetovv_aidyn@mail.ru>
2024-07-29 21:18:49 +0500

    pyramid-generator-25.js fix

--- learn-intoductory-js-building-a-pyramid-generator/pyramid-generator-25.js
+++ learn-intoductory-js-building-a-pyramid-generator/pyramid-generator-25.js
@@ -4,7 +4,8 @@ let rows = ["Naomi", "Quincy", "CamperChan"];
 
 // Step 25
 // Now you are ready to move onto the next set of array lessons.
+// Remove all of your code from the previous step.
 //
-//     Remove all of your code from the previous step.
 
 console.log(rows);
+
