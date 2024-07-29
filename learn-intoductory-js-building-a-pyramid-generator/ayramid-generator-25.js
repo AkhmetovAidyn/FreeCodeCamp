74780d1c2e2cef6129df0ad5feb88c6e5650775b
AkhmetovAidyn <ahmetovv_aidyn@mail.ru>
2024-07-29 21:06:11 +0500

    pyramid-generator-25.js

---
+++ learn-intoductory-js-building-a-pyramid-generator/ayramid-generator-25.js
@@ -0,0 +1,10 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+// Step 25
+// Now you are ready to move onto the next set of array lessons.
+//
+//     Remove all of your code from the previous step.
+
+console.log(rows);
