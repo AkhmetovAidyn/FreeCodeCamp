b3f79b0f55de0293572a34d2bc2b370293b24557
AkhmetovAidyn <ahmetov.aidyn1988@mail.ru>
2024-08-04 00:45:08 +0500

    store-multiple-values-in-one-variable-using-js-arrays.js

--- Basic JS/store-multiple-values-in-one-variable-using-js-arrays.js
+++ Basic JS/store-multiple-values-in-one-variable-using-js-arrays.js
@@ -8,4 +8,4 @@
 // Modify the new array myArray so that it contains both a string and a number (in that order).
 
 // Only change code below this line
-const myArray = ["str", 23];
+const myArray = ["str", 23, 'str2'];
